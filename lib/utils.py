4485b60356f236bd6cc8bf7e3c75710186b201cc
mistangl <m.stangl@schoenenberger.de>
2026-03-23 12:41:23 +0100

    fix einer utils funktion für den Config Pfad

--- lib/utils.py
+++ lib/utils.py
@@ -23,7 +23,7 @@ def get_cfg_dir() -> Path:
     env_cfg = os.environ.get('PV_CFG')
     if env_cfg:
         return Path(env_cfg)
-    return get_cfg_dir()
+    return Path(__file__).parent.parent / "cfg"
 
 
 # =============================================================================
